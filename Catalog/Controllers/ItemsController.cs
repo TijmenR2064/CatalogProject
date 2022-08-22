613ad1e56826dfa1cdeb3d5f66e190b39efd485d
Tijmen <tijmen2063@gmail.com>
2022-08-23 00:08:24 +0200

    get+get(id)

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Catalog.Controllers
+{
+    //Get /items
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        //Get /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
